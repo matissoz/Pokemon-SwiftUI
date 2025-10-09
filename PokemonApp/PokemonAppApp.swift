8037241d707a8a8da83b74b46951e06b301d934d
matissoz <matiss.zelmens@icloud.com>
2025-10-09 21:25:26 +0300

    Initial Commit

---
+++ PokemonApp/PokemonAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  PokemonAppApp.swift
+//  PokemonApp
+//
+//  Created by Matīss Zelmens on 09/10/2025.
+//
+
+import SwiftUI
+
+@main
+struct PokemonAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
